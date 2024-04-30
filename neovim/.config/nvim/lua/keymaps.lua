ccbdb4c8d2e824b37da61ad098a168ad69e52652
Sagiring <dieter.anagram_0t@icloud.com>
2024-04-30 11:15:23 +0800

    set indent and auto-install in treesitter

--- neovim/.config/nvim/lua/keymaps.lua
+++ neovim/.config/nvim/lua/keymaps.lua
@@ -3,3 +3,6 @@ local keymap = vim.keymap
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 keymap.set("n", "<leader>t", ":NvimTreeFocus<CR>")
+
+-- nvim-treesitter
+keymap.set("n", "<C-M-l>", "gg=G")
