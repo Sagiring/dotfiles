f183e8b80dd55091c25c3e54194ff35e63768299
zhang.jingming <zhang.jingming@hellogroup.com>
2024-12-04 19:16:41 +0800

    update

--- script/script/moa-invoker.sh
+++ script/script/moa-invoker.sh
@@ -3,6 +3,6 @@ cd /Users/sagiring/tools/moa-invoker
 # 启动虚拟环境
 source /Users/sagiring/tools/moa-invoker/.venv/bin/activate
 # 启动脚本
-output=$(exec /Users/sagiring/tools/moa-invoker/.venv/bin/python3 /Users/sagiring/tools/moa-invoker/script.py "$@")
+output=$(exec /Users/sagiring/tools/moa-invoker/.venv/bin/python3 /Users/sagiring/tools/moa-invoker/script.py $@)
 echo "$output" | head -n3 
 echo "$output" | tail -n +4 | highlight -O xterm256 --syntax json --style catppuccin-Macchiato
