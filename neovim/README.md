aec273a201c83cff536ab35281f5b58f3afd6da1
zhang.jingming <zhang.jingming@hellogroup.com>
2024-12-05 17:48:49 +0800

    清理临时文件

--- neovim/README.md
+++
@@ -1,3 +0,0 @@
-You can easily 'stow neovim', then use ':PluginInstall' to automatically install all the plugins.
-But some plugins need to install dependencies, you need to install them manually.
-For instance, you need to use ":Mason" to install LSPs, if you want to use "nvim-cmp".
--- script/script/moa-invoker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-cd /Users/sagiring/tools/moa-invoker
-# 启动虚拟环境
-source /Users/sagiring/tools/moa-invoker/.venv/bin/activate
-# 启动脚本
-output=$(exec /Users/sagiring/tools/moa-invoker/.venv/bin/python3 /Users/sagiring/tools/moa-invoker/script.py $@)
-echo "$output" | head -n3 
-echo "$output" | tail -n +4 | highlight -O xterm256 --syntax json --style catppuccin-Macchiato
